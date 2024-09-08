687df6d6275be1b56b67a35b3b5fd82b357df003
Marimariia <eduardaqqquintino@gmail.com>
2024-09-07 23:15:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e personalidade vibrante, Rayssa é um ícone da nova geração do skate.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros e o mundo esportivo com sua habilidade, determinação e carisma. Bicampeã olímpica, ela é a maior medalhista da história do Brasil nos Jogos Olímpicos, com um currículo invejável que inclui diversas medalhas em competições nacionais e internacionais.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "judô judo judoka"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou no cenário internacional com suas técnicas precisas e determinação. Com diversas medalhas em competições importantes, ela é um orgulho para o judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "ginastica ginástica ouro"
+      },
+];
+
